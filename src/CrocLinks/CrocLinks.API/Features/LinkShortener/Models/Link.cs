ee0acc71a55caf8633978d400de35a8afdf429ee
Sam Day <samday03@gmail.com>
2021-10-22 19:20:05 +0100

    added database models and context

---
+++ src/CrocLinks/CrocLinks.API/Features/LinkShortener/Models/Link.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace CrocLinks.API.Features.LinkShortener.Models
+{
+    public partial class Link
+    {
+        public long LinkId { get; set; }
+        public DateTime LinkCreatedDate { get; set; }
+        public string LinkToken { get; set; }
+        public string OriginalLink { get; set; }
+        public string ShortenedLink { get; set; }
+        public DateTime? LinkExpiryDate { get; set; }
+    }
+}
---
+++ src/CrocLinks/CrocLinks.API/Features/LinkShortener/Models/LinkShortenerContext.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace CrocLinks.API.Features.LinkShortener.Models
+{
+    public partial class LinkShortenerContext : DbContext
+    {
+        public LinkShortenerContext()
+        {
+        }
+
+        public LinkShortenerContext(DbContextOptions<LinkShortenerContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Link> Links { get; set; }
+        public virtual DbSet<LinkUsage> LinkUsages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Latin1_General_CI_AS");
+
+            modelBuilder.Entity<Link>(entity =>
+            {
+                entity.ToTable("Link");
+
+                entity.Property(e => e.LinkId).HasColumnName("LinkID");
+
+                entity.Property(e => e.LinkCreatedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.LinkExpiryDate).HasColumnType("datetime");
+
+                entity.Property(e => e.LinkToken)
+                    .IsRequired()
+                    .HasMaxLength(8)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.OriginalLink)
+                    .IsRequired()
+                    .HasMaxLength(1000)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ShortenedLink)
+                    .IsRequired()
+                    .HasMaxLength(128)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<LinkUsage>(entity =>
+            {
+                entity.ToTable("LinkUsage");
+
+                entity.HasIndex(e => e.LinkId, "IX_LinkUsage_01");
+
+                entity.Property(e => e.LinkUsageId).HasColumnName("LinkUsageID");
+
+                entity.Property(e => e.LinkClickedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.LinkId).HasColumnName("LinkID");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
